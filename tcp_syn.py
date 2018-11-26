e8eeb005015eb4dad892010d3386ff646d2fa94f
aqeebhussain <aqeebrajhussain@gmail.com>
2018-11-26 14:24:23 +0000

    Working version of tcp_syn without functions

--- tcp_syn.py
+++ tcp_syn.py
@@ -7,8 +7,11 @@ from struct import *
 
 # Global list to store the error messages
 msg = []
+
+# template socket which will be passed to functions
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 
+# DOCSTRING: SOCKET CREATION using try and catch error handling
 def createSock(s):
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
@@ -16,7 +19,9 @@ def createSock(s):
 		print 'Socket could not be created. Error code: ' + str(msg[0]) + ' Message ' + str(msg[1])
 	return s
 
+# Calling of the function to create a socket
 createSock(s)
+
 # Checksum which will be passed to verify - Contained in function with storage and access to a global list
 def checksum(msg):
 	s = 0
@@ -27,13 +32,6 @@ def checksum(msg):
 	s = ~s & 0xffff
 
 	return s
-
-# Raw socket creation
-#try:
-#	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-#except socket.error as msg:
-#	print 'Socket could not be created. Error Code: ' + str(msg[0]) + ' Message ' + str(msg[1])
-#	sys.exit(1)
 
 createSock(s)
 
@@ -60,11 +58,13 @@ daddr = socket.inet_aton(dest_ip)
 
 ihl_version = (version << 4) + ihl
 
+# IP Header packed up 
 ip_header = pack('!BBHHHBBH4s4s', ihl_version, tos, tot_len, id, frag_off, ttl, protocol, check, saddr, daddr)
 
+
 # source port
-source = 556
-# source port
+source = 5456
+# destination port
 dest = 80
 seq = 0
 ack_seq = 0
@@ -74,40 +74,53 @@ doff = 5
 fin = 0
 # Activation of syn packet to enable syn scan
 syn = 1
+# Reset flag
 rst = 0
+# Push flag
 psh = 0
+# ACKnowledgement flag
 ack = 0
+# Urgent flag
 urg = 0
+# Window size of the scan
 window = socket.htons(5480)
 check = 0
+# Urgent pointer
 urg_ptr = 0
 
 offset_res = (doff << 4) + 0
 tcp_flags = fin + (syn << 1) + (rst << 2) + (psh << 3) + (ack << 4) + (urg << 5)
 
+# TCP Header packed
 tcp_header = pack('!HHLLBBHHH', source, dest, seq, ack_seq, offset_res, tcp_flags, window, check, urg_ptr)
 
 #pseudo header fields
 source_address = socket.inet_aton(source_ip)
 dest_address = socket.inet_aton(dest_ip)
 placeholder = 0
+#TCP Header assigned with TCP protocol
 protocol = socket.IPPROTO_TCP
 tcp_length = len(tcp_header)
 
 psh = pack('!4s4sBBH', source_address, dest_address, placeholder, protocol, tcp_length)
 psh += tcp_header
 
+# Checksum function using push flag
 tcp_checksum = checksum(psh)
 
 tcp_header = pack('!HHLLBBHHH', source, dest, seq, ack_seq, offset_res, tcp_flags, window, tcp_checksum, urg_ptr)
 
+# Function:
+	# Take IP header and TCP header as function calls 
+	# add result of both functions 
+	# return the packet
+	#
+	#
+
 packet = ip_header + tcp_header
-
-s.sendto(packet, (dest_ip, 0))
-result = s.connect_ex(('127.0.0.1', 80))
-
-if result == 0:
-	print "Port is open"
-else:
-	print "Port is not open"
-
+# Attempt to check if data has actually been sent
+try:
+	result = s.sendto(packet, (dest_ip, 0))
+except Exception as e:
+	print(e)
+	listeners.remove(1)
