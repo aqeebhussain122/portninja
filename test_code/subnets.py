19ddd62ac1402fa6f792996bb9ae64036681c4c4
aqeebLinuxMint <aqeebrajhussain@gmail.com>
2019-01-14 21:56:49 +0000

    Test programs to complete and embed into main component

---
+++ test_code/subnets.py
@@ -0,0 +1,6 @@
+# Take input from first command line argument and calculate the given number after /
+# Limit the number selection from 1 to 32
+# Map a specific amount of hosts to each value e.g. /30 = 2
+# Maximum number of elements should be based upon the subnet number
+# Store all subnet values in a list and map their values to an if condition
+
