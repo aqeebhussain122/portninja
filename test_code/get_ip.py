bda2e368fac814222fceea909bc67e0a7e51a4a1
Aqeeb Hussain <aqeebrajhussain@gmail.com>
2019-06-07 17:13:24 +0100

    get ip code to be formed in function and tested ffs

--- test_code/get_ip.py
+++ test_code/get_ip.py
@@ -1,5 +1,9 @@
 import socket
 
-hostname = socket.gethostname()
-ip_addr = socket.gethostbyname(hostname)
-print("{} {}".format(hostname, ip_addr))
+# UDP Packet
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Connect to non-existent source
+sock.connect(('10.0.0.255', 1))
+# Retrieve the IP from the connected socket
+IP = sock.getsockname()[0]
+print(IP)
