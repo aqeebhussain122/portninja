8d41bf42f1f3253693f490ff98374e63ee8b5d78
Aqeeb Hussain <aqeebrajhussain@gmail.com>
2019-06-08 22:56:55 +0100

    python 3 changes

--- test_code/main.py
+++ test_code/main.py
@@ -13,50 +13,49 @@ import craft
 
 def main():
 	# Raw socket for raw packet
-        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-        ip_details = ip.get_ip()
-			
-	# Removed source port from main parser	
-        parser = argparse.ArgumentParser(description='SYN Scan and flood tool which forms raw packets taking required IP addresses and port numbers')
-        parser.add_argument("destination_ip", help='Destination IP address to form packet', type=str)
-        parser.add_argument("source_port", help='Source port to form packet', type=int)
-        parser.add_argument("destination_port", help='Destination port to form packet', type=int)
-        parser.add_argument("-f", "--flood", help="SYN Flood option to send arbituary number of packets to flood device or network", type=int)
+	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+	ip_details = ip.get_ip()
+
+	parser = argparse.ArgumentParser(description='SYN Scan and flood tool which forms raw packets taking required IP addresses and port numbers')
+	parser.add_argument("destination_ip", help='Destination IP address to form packet', type=str)
+	parser.add_argument("source_port", help='Source port to form packet', type=int)
+	parser.add_argument("destination_port", help='Destination port to form packet', type=int)
+	parser.add_argument("-f", "--flood", help="SYN Flood option to send arbituary number of packets to flood device or network", type=int)
         # Ports switch which takes a list of ports and stores them for processing
         # Elements in ports list to be accessed one by one and given to port check function
-        parser.add_argument("-p", "--ports", help="Port switch  which will take a number of ports and scan them against destination ip", type=int, nargs='+')
-        parser.add_argument("-s", "--sweep", help="Ping sweep through the network", action='store_true')
-        args = parser.parse_args()
+	parser.add_argument("-p", "--ports", help="Port switch  which will take a number of ports and scan them against destination ip", type=int, nargs='+')
+	parser.add_argument("-s", "--sweep", help="Ping sweep through the network", action='store_true')
+	args = parser.parse_args()
 	ports.portNumLimit(args.source_port)
-        ports.portNumLimit(args.destination_port)
-        ip_header = craft.ipCreate(ip_details, args.destination_ip)
-        tcp_header = craft.tcpCreate(ip_details, args.destination_ip, args.source_port, args.destination_port)
-        packet = ip_header + tcp_header
-        ports.portNumLimit(args.source_port)
-        ports.portNumLimit(args.destination_port)
-        print(('IP Address is: ' + args.destination_ip))
-        ports.TCPportCheck(args.destination_ip, args.destination_port)
-        print('Performing banner grab')
-        ports.TCPbannerGrab(args.destination_ip, args.destination_port)
+	ports.portNumLimit(args.destination_port)
+	ip_header = craft.ipCreate(ip_details, args.destination_ip)
+	tcp_header = craft.tcpCreate(ip_details, args.destination_ip, args.source_port, args.destination_port)
+	packet = ip_header + tcp_header
+	ports.portNumLimit(args.source_port)
+	ports.portNumLimit(args.destination_port)
+	print(('IP Address is: ' + args.destination_ip))
+	ports.TCPportCheck(args.destination_ip, args.destination_port)
+	print('Performing banner grab')
+	ports.TCPbannerGrab(args.destination_ip, args.destination_port)
 
         # Checks for additional switches after the primary port check and banner grab
-        if args.flood:
-                print("Flood option selected: Sending packets...")
-                i = 0
-                value = int(args.flood)
-                while i < value:
-                        i += 1
-                        print(("Packets sent: {}".format(i)))
-                        result = s.sendto(packet, (args.destination_ip, 0))
-        if args.ports:
-                for port in args.ports:
-                        check_success = ports.TCPportCheck(args.destination_ip, port)
-                        check_banner = ports.TCPbannerGrab(args.destination_ip, port)
+	if args.flood:
+		print("Flood option selected: Sending packets...")
+		i = 0
+		value = int(args.flood)
+		while i < value:
+			i += 1
+			print(("Packets sent: {}".format(i)))
+			result = s.sendto(packet, (args.destination_ip, 0))
+	if args.ports:
+		for port in args.ports:
+			check_success = ports.TCPportCheck(args.destination_ip, port)
+			check_banner = ports.TCPbannerGrab(args.destination_ip, port)
 
         # Ping sweep all ports on a machine - freezing up on the first attempt 
-        if args.sweep:
-            for i in range(1, 65565):
-                sweep_check = ports.TCPportCheck(args.destination_ip, i)
+	if args.sweep:
+		for i in range(1, 65565):
+			sweep_check = ports.TCPportCheck(args.destination_ip, i)
 
 if __name__ == '__main__':
         main()
