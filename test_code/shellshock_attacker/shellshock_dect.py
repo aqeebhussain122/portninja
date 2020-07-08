ad2d74e49963516a2adbbee32d722e8bd63116cc
Aqeeb <aqeebrajhussain@gmail.com>
2020-07-08 23:39:52 +0100

    Final commit of tonight, onto the sesh

--- test_code/shellshock_attacker/shellshock_dect.py
+++ test_code/shellshock_attacker/shellshock_dect.py
@@ -5,20 +5,26 @@ import sys
 Use an internal HTTP list of ports which will be scanned for being open/closed with 1 and 0
 '''
 def scan_http(ip_addr):
-    # Using these to check if the ports are open or not
+    # Using these to check how many ports are open/closed
     ports_open = 0
     ports_not_open = 0
-    
+   
+    '''
+    Don't need these
     http_port_80 = 80 # Just numbers
     http_port_88 = 88
     http_port_443 = 443
     http_port_8080 = 8080
     '''
+
+    '''
     These are the main http ports which need to be split to two more sublists of open_http_ports and closed_http_ports.
     This can be used further for processing in the other functions
     '''
-    http_ports = [80,88,443,8080]
+    #http_ports = [80,88,443,8080]
+    http_ports = (80,88,443,8080)
     # Sublists for the open and closed ports
+    #http_open_ports = []
     http_open_ports = []
     http_closed_ports = []
     #http_ports = [http_port_80,http_port_88,http_port_443,http_port_8080]
@@ -48,18 +54,29 @@ def scan_http(ip_addr):
         else:
             pass
 
-    '''
-    string_http_open_ports = [str(open_ports) for open_ports in http_open_ports]
-    encoded_open_ports = ",".join(string_http_open_ports)
-    print("Open ports are {}".format(encoded_open_ports))
-    print("Closed ports are: {}".format(http_closed_ports))        
-    '''
-    
-    # If the list is empty
+    # If there's no open ports
     if not http_open_ports:
-        print("No ports open, ima dip")
-    else:
-        print("I found some ports")
+        print("No ports open, aight imma head out")
+        sys.exit(1)
+        '''
+        Printing out the ports is pointless because ports.py does it 
+        for i in range(len(http_open_ports)):
+            print("Open: {}".format(http_open_ports[i]))
+        
+        for i in range(len(http_closed_ports)):
+            print("Closed: {}".format(http_closed_ports[i]))
+        '''
+
+        '''
+        string_http_open_ports = [str(open_ports) for open_ports in http_open_ports]
+        # Replaced the [] with a space
+        encoded_open_ports = ",".join(string_http_open_ports)
+        string_http_closed_ports = [str(closed_ports) for closed_ports in http_closed_ports]
+        encoded_closed_ports = ",".join(string_http_closed_ports)
+        print("Open port(s) are {}".format(encoded_open_ports))
+        print("Closed port(s) are: {}".format(encoded_closed_ports))
+        '''
+    # If the list is empty
         #print("Successful {}".format(successful))
         #print("Not successful {}".format(not_successful))
 
@@ -83,22 +100,37 @@ def scan_http(ip_addr):
     print(single_check_2)
     '''
     # This counter is to check for the 
-    return ports_open
-
+    return http_open_ports
+#
     # Return the list elements of an 
     #return error_exit
 
 #def cgi_bin_detector(url, port):
 
 def main():
-    # This is the output of the IP address, not the function return
-    #http_scan = scan_http("192.168.0.100")
+    # IP address is separate variable instead of being part 
     ip_addr = sys.argv[1]
 
+    # Contains the open ports which are the return values from this function
+    '''
+    Get the open ports, make sure they're integers and then pass them through to the needed functions
+    '''
     http_scan = scan_http(ip_addr)
+    #open_ports_amount = len(http_scan)
+    for i in range(len(http_scan)):
+        print("Open HTTP ports are: {}".format(http_scan[i]))
+        
+        # Testing to see if the list elements in http_scan can be treated as ports
+        #ports.TCPportCheck(ip_addr, http_scan[i])
+
     print("Target IP address: {}".format(ip_addr))
+    #print("Function return: {}\nFunction data type: {}".format(http_scan, type(http_scan)))
 
-
+    '''
+    list to integer procedure (Maybe)
+        1. Turn the list elements into strings
+        . Turn the strings into ints
+    '''
     # This if/else logic needs to be sorted out
     '''
     if(http_scan == 0):
@@ -113,8 +145,9 @@ def main():
 
     '''
     We want the return code of http_scan to proceed accordingly.
-    By extracting the ports_open counter and measuring the if/else logic of 0 http ports being open
+    By extracting the ports_open counter and measuring the if/else logic of 0 http ports being ope
     then the program should exit from trying to identify the presence of a cgi-bin directory.
+    Instead of doing this between the function and main, it's better to contain all error handling in the function 
     '''
 main()
         
