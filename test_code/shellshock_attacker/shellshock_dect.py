73cc8b2b5835baeaf80edd05b7633b2c53ac3cbb
Aqeeb <aqeebrajhussain@gmail.com>
2020-07-10 00:30:10 +0100

    Stucked

--- test_code/shellshock_attacker/shellshock_dect.py
+++ test_code/shellshock_attacker/shellshock_dect.py
@@ -1,7 +1,6 @@
 import ports
 import sys
 import requests
-import cgi_crawler
 
 '''
 Make an HTTP request to the available ports and append /cgi-bin and get the return code of it
@@ -79,23 +78,79 @@ def scan_http(ip_addr):
     if not http_open_ports:
         print("No ports open, aight imma head out")
         sys.exit(1)
-
     return http_open_ports
 
+    
 '''
-IP address and port as input parameters
+Checks the wordlist and whichever words return 200, this function will return it
 '''
-def cgi_bin_crawler(ip_addr, port):
-    '''
-    URL is extracted from the function which is then given to the checkPath function
-    '''
-    cgi_url = extract_cgi_url(ip_addr, port)
-    print("CheckPath function output: {}".format(cgi_crawler.checkPath(cgi_url)))
+def check_wordlist(ip_addr, port, word):
+    base_url = extract_cgi_url(ip_addr, port)
+    word_response = requests.get(base_url + '/' + word).status_code
+    #word_url = requests.get(base_url + '/' + word)
+    # Executes the wordlist 
+    word_url_extract = requests.Request('GET', '{}/{}'.format(base_url, word))
+    prep = word_url_extract.prepare()
+    word_url = prep.url
+    #print("CheckPath function output: {}".format(cgi_crawler.checkPath(cgi_target_url)))
+    #print(word_url_extract)
+    #print(word_url)
+    if(word_response == 200):
+        print("cgi-bin target: {}".format(word_url))
+    else:
+        pass
+        #print("Nothing found")
+        #sys.exit(1)
+    return word_response
 
-    return cgi_url
+
+
+def execute_wordlist(ip_addr, port, wordlist):
+    '''
+    1. Open the file
+    2. Read the file and check how many lines to check
+    3. Open the for loop properly in which the crafted url can use the check_wordlist to see if a 200 entry appears
+    4. (Do error handling of everything once the core code is written
+    '''
+
+    ''' SO FAR JUST A STATIC LINK IS CREATED IT SEEMS! '''
+    base_url = extract_cgi_url(ip_addr, port)
+    with open(wordlist) as file:
+        # This reads the file
+        words_to_check = file.read().strip().split('\n')
+    print("Total words to check: {}".format(len(words_to_check)))
+    for words in range(len(words_to_check)):
+        word_response = requests.get(base_url + '/' + words_to_check[words])
+        word_url_extract = requests.Request('GET', '{}/{}'.format(base_url,words_to_check[words]))
+        prep = word_url_extract.prepare()
+        word_url = prep.url
+        print(word_url)
+        print(word_response)
+        '''
+        This test just didn't even work...
+        if(word_response == 200):
+            print("Found: {}".format(word_url))
+            sys.exit(0)
+        '''
+
+    '''
+    Or this one
+    if(word_response == 200):
+        print("Found: {}".format)
+    '''
+        #print(check_wordlist(ip_addr, port, words_to_check[words]))
+        # Print the words from the list first and then try to make a GET request in this 
+        #print(words_to_check[words])
+        # This prints the actual words in the wordlist
+        #check_words = check_wordlist(ip_addr, port, words_to_check[word])
+        #print(check_words)
+        #print(word_url)
+    return word_response
+
 
 def main():
     ip_addr = sys.argv[1]
+    wordlist = sys.argv[2]
     print("Target IP address: {}".format(ip_addr))
     # Contains the open ports which are the return values from this function
     open_http_ports = scan_http(ip_addr)
@@ -104,14 +159,18 @@ def main():
     # Looping through by the number of open ports
     for port in range(len(open_http_ports)):
         print("Open HTTP ports are: {}".format(open_http_ports[port]))
-        target_url = extract_cgi_url(ip_addr, open_http_ports[port])
-        print("Target URL: {}".format(target_url))
+        target_base_url = extract_cgi_url(ip_addr, open_http_ports[port])
+        print("Target URL: {}".format(target_base_url))
         target_status_code = get_status_code(ip_addr, port)
         print("HTTP GET Request status code: {}".format(target_status_code))
         target_port = open_http_ports[port]
         print("The target port for cgi-bin detection is {}".format(target_port))
         print("--------------------------------\nStarting web crawler")
-        web_crawler_url = cgi_bin_crawler(ip_addr, target_port)
-        print("Web crawler URL: {}".format(web_crawler_url))
-
+        print("Web crawler URL: {}".format(target_base_url))
+        #target_wordlist_response_code = check_wordlist(ip_addr, port, wordlist)
+        #target_wordlist_response_code = check_wordlist(ip_addr, port, wordlist)
+        #target_wordlist_response_code = check_wordlist(ip_addr, port, 'statu')
+        #print("Wordlist checker response code: {}".format(target_wordlist_response_code))
+        
+        print("Execute wordlist URL: {}".format(execute_wordlist(ip_addr, port, wordlist)))
 main()
