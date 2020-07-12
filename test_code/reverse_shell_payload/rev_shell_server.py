de2fa7394bb733bb402a224791e6e1f1ccfbf855
Aqeeb <aqeebrajhussain@gmail.com>
2020-07-12 16:55:17 +0100

    Experimentation

---
+++ test_code/reverse_shell_payload/rev_shell_server.py
@@ -0,0 +1,30 @@
+''' This program listens on a port on this machine (Attacker) and waits for the client to connect back. Once the connection comes in. The shell initiates '''
+import socket
+import sys
+
+''' Locally bind a socket to port so it can receive data '''
+def listen_sock(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(('0.0.0.0', port))
+    except socket.error as msg:
+        print("Bind failed. Pagan...")
+        sys.exit()
+    print("Binding is done")
+    
+    listen_conns = s.listen(3)
+    print("Listening connections: {}".format(listen_conns))
+    print('Socket is listening...')
+    # Wait infinitely
+    while 1:
+        conn, addr = s.accept()
+        print("Connected")
+
+    s.close()
+
+    return addr
+
+def main():
+    listen_sock(3333)
+
+main()
