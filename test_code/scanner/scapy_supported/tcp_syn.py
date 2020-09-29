50844e1f5cf4f01c0c030375edc8ca8c842269eb
Aqeeb <aqeebrajhussain@gmail.com>
2020-09-29 22:39:27 +0100

    Troubleshooting to do

--- test_code/scanner/scapy_supported/tcp_syn.py
+++ test_code/scanner/scapy_supported/tcp_syn.py
@@ -5,12 +5,25 @@ import random
 def send_syn(target_ip, port):
     # Generate a raw packet on our side to send to the target 
     src_port = random.randint(1, 65535)
-    packet = sr1(IP(dst=target_ip)/TCP(sport=src_port, dport=int(port),flags="S"))
+
+    resp_packet = sr1(IP(dst=target_ip)/TCP(sport=src_port, dport=int(port),flags="S"))
     # If a syn acknowledgement packet is recieved then it's open for sure
-    if packet.getlayer(TCP).flags == "SA":
+
+    ''' Error happening because port is coming back with an ICMP error, this can be further profiled with an ACK scan '''
+    # IP / ICMP 192.168.59.130 > 192.168.59.129 dest-unreach port-unreachable / IPerror / TCPerror
+    """
+    if resp_packet == None:
+        print("connect failed")
+
+    if resp_packet.getlayer(TCP).flags == "SA":
         print(port, "is open")
-    
-    return packet
+    elif resp_packet.getlayer(TCP).flags == "RA":
+        print(port, "is closed")
+    else:
+        pass
+    """
+
+    return resp_packet
 
 def send_fin():
     return
