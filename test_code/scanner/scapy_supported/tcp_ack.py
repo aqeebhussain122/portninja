1e7c8aad7c7443e4ed131458c92e6be26a9daabc
Aqeeb <aqeebrajhussain@gmail.com>
2020-10-01 09:06:21 +0100

    code fixes

--- test_code/scanner/scapy_supported/tcp_ack.py
+++ test_code/scanner/scapy_supported/tcp_ack.py
@@ -7,14 +7,18 @@ def send_ack(ip, port, result = 1):
         p = IP(dst=ip)/TCP(sport=src_port, dport=port, flags='A', seq=65782)
         resp = sr1(p, timeout=2)
         # If no reply is sent back in the response then call it unreachable.
-        if str(type(resp)) == "<type 'NoneType'>":
+        #if str(type(resp)) == "<type 'NoneType'>": - Inefficient
+        #    result = 1
+        if resp == None:
             result = 1
 
         # If the response as a TCP packet then 
-        elif resp.haslayer(TCP):
+        if resp.haslayer(TCP):
             if resp.getlayer(TCP).flags == 0x4:
                 result = 0
-            elif (int(resp.getlayer(ICMP).type)==3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+
+        if resp.haslayer(ICMP):
+            if (int(resp.getlayer(ICMP).type)==3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
                 result = 1
     except Exception as e:
         pass
