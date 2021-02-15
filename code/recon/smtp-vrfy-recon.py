6e42333bf297c27a7c940c0ac684f0993fa4cbc5
Aqeeb H <35127065+aqeebhussain122@users.noreply.github.com>
2021-02-15 12:44:33 +0000

    Update smtp-vrfy-recon.py

--- code/recon/smtp-vrfy-recon.py
+++ code/recon/smtp-vrfy-recon.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-
 import socket
 import sys
+import time
 
 target = sys.argv[1]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+print("Connecting to SMTP server")
 sock.connect((target, 25))
 banner = sock.recv(1024)
 
 print(banner)
-sock.send(b'VRFY root \r\n')
-answerUsername = sock.recv(1024)
 
-print(answerUsername)
+with open('test_list.txt') as fp:
+	lines = fp.readlines()
+	for line in lines:
+		sock.send(b'VRFY %b' % line.encode())
+		time.sleep(5)
+		answerUsername = sock.recv(1024)
+		print(answerUsername)
