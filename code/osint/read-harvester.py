d57ef5529cae2178581489f8f9e798da800baa9b
Aqeeb H <35127065+aqeebhussain122@users.noreply.github.com>
2021-09-20 10:29:49 +0100

    Parse JSON reporting output from theHarvester

--- code/osint/read-harvester.py
+++ code/osint/read-harvester.py
@@ -1,6 +1,15 @@
 import json
 import sys
 
+def read_json(json_harv_file):
+    with open('{}'.format(json_harv_file), 'r') as json_file:
+        data = json_file.read()
+    
+    obj = json.loads(data)
+    if not json_harv_file:
+        raise Exception("No file was provided")
+
+    return obj
 
 def json_hosts(json_harv_file):
     with open('{}'.format(json_harv_file), 'r') as json_file:
@@ -26,46 +35,44 @@ def json_hosts(json_harv_file):
 
 
 def json_emails(json_harv_file):
-    with open('{}'.format(json_harv_file), 'r') as json_file:
-        data = json_file.read()
 
-    obj = json.loads(data)
+    obj = read_json(json_harv_file)
     if "emails" not in obj:
         print("No emails found")
-    else:
-        pass
+    
+    elif "emails" in obj:
+        emails = obj['emails']
+        for i in range(len(emails)):
+            print(emails[i])
+    
         # Insert email parsing here
     #print(emails)
 
 def json_urls(json_harv_file):
-    with open('{}'.format(json_harv_file), 'r') as json_file:
-        data = json_file.read()
-        obj = json.loads(data)
-        print(obj['asns'])
-        if "interesting_urls" not in obj:
-            print("No URLs found")
-        
+    obj = read_json(json_harv_file)
+    if "interesting_urls" not in obj:
+        print("No URLs found")
+    
+    elif "interesting_urls" in obj:
         interesting_urls = obj['interesting_urls']
         for i in range(len(interesting_urls)):
             print(interesting_urls[i])
 
 
 def json_asns(json_harv_file):
-    with open('{}'.format(json_harv_file), 'r') as json_file:
-        data = json_file.read()
-        obj = json.loads(data)
-        if "asns" not in obj:
-            print("No ASNs found")
-
+    obj = read_json(json_harv_file)
+    if "asns" not in obj:
+        print("No ASNs found")
+    
+    elif "asns" in obj:
         asns = obj['asns']
         for i in range(len(asns)):
             print(asns[i])
 
-print("Discovered hosts")
-json_hosts('harvester.json')
-print("Discovered emails")
-json_emails('harvester.json')
+json_file = sys.argv[1]
 print("Discovered URLs")
-urls = json_urls('harvester.json')
-print("Discovered ASNs")
-asns = json_asns('harvester.json')
+urls = json_urls(json_file)
+print("\nDiscovered ASNs")
+asns = json_asns(json_file)
+print("\nDiscovered emails")
+email = json_emails(json_file)
