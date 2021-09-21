4e6d94256c21ae4ce7260eac1225f03eb47c927f
Aqeeb H <35127065+aqeebhussain122@users.noreply.github.com>
2021-09-21 10:22:43 +0100

    Update read-harvester.py

--- code/osint/read-harvester.py
+++ code/osint/read-harvester.py
@@ -12,31 +12,28 @@ def read_json(json_harv_file):
     return obj
 
 def json_hosts(json_harv_file):
-    with open('{}'.format(json_harv_file), 'r') as json_file:
-        data = json_file.read()
 
-    obj = json.loads(data)
-# Available keys
-    print(obj.keys())
-
+    obj = read_json(json_harv_file)
     hosts = obj['hosts']
 
     for i in range(len(hosts)):
         l = hosts[i].split(':')
-        #print(l)
         hostnames = l[0]
         ip_addrs = l[1:]
         if not hostnames:
             print('No hostname found')
+
+        elif not ip_addrs:
+            print('No IP found for {}'.format(hostnames))
+
         elif len(ip_addrs) == 0:
             print('No IP found for {}'.format(hostnames))
+
         elif len(ip_addrs) > 0:
             ip_addrs_str = ' '.join([str(item) for item in ip_addrs])
             print("Hostname(s): {} / IP address(es): {}".format(hostnames, ip_addrs_str))
 
-
 def json_emails(json_harv_file):
-
     obj = read_json(json_harv_file)
     if "emails" not in obj:
         print("No emails found")
@@ -45,19 +42,21 @@ def json_emails(json_harv_file):
         emails = obj['emails']
         for i in range(len(emails)):
             print(emails[i])
-    
-        # Insert email parsing here
-    #print(emails)
 
 def json_urls(json_harv_file):
     obj = read_json(json_harv_file)
-    if "interesting_urls" not in obj:
+    if "interesting_urls" not in obj and "urls" not in obj:
         print("No URLs found")
-    
+
     elif "interesting_urls" in obj:
         interesting_urls = obj['interesting_urls']
         for i in range(len(interesting_urls)):
             print(interesting_urls[i])
+
+    elif "urls" in obj:
+        urls = obj['urls']
+        for i in range(len(urls)):
+            print(urls[i])
 
 
 def json_asns(json_harv_file):
