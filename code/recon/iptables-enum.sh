098fa006e6d4ec1feab87d51d1c711913cd53091
Aqeeb <aqeeb@aqeeb.com>
2021-11-09 10:22:06 +0000

    IP address and port don't need to be hardcoded

--- code/recon/iptables-enum.sh
+++ code/recon/iptables-enum.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # IP address and port need to be hardcoded
-timeout 1 bash -c 'cat < /dev/null > /dev/tcp/$ip/$port'
+#timeout 1 /bin/bash -c 'cat < /dev/null > /dev/tcp/192.168.1.11/8080'
+
+ip=$1
+port=$2
+
+#timeout 1 /bin/bash -c 'cat < /dev/null > /dev/tcp/192.168.1.11/8080'
+`timeout 1 cat < /dev/null > /dev/tcp/$ip/$port`;
 
 code=$?
+echo "Error code is: $code"
 if [ $code -eq 124 ]; then
         echo "IPTables is likely applied on the specified port"
 elif [ $code -eq 1 ]; then
