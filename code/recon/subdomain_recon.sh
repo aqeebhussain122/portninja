9608d68b5509f49b6069f94a4676223cdb49b32b
Aqeeb Hussain <aqeebrajhussain@gmail.com>
2022-06-10 14:40:05 +0100

    added wayback subdomain checks

--- code/recon/subdomain_recon.sh
+++ code/recon/subdomain_recon.sh
@@ -38,6 +38,11 @@ echo "Grabbing further info in normal enum mode"
 $check_file enum -d $1 -o "amassoutput"
 }
 
+root_domain=$1
+
+echo "Performing subdomain checks via Wayback"
+curl -s "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" |sort| sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^www\.//' | uniq
+
 amass_installed=$(check_amass_installed)
 echo $amass_installed
 
