ad5d1e47a504311329c06455d66a19f0edd05045
Aqeeb <aqeeb@aqeeb.com>
2022-06-12 00:16:54 +0100

    Rewritten logic. Ubuntu based install only to cater for Kali and other Ubuntu systems.

--- code/recon/subdomain_recon.sh
+++ code/recon/subdomain_recon.sh
@@ -6,21 +6,30 @@
 
 check_amass_installed()
 {
-	amasspath=$(find / -name amass 2>/dev/null)
+	amasspath=$(find / -type f -executable -name amass 2>/dev/null)
 	for i in $amasspath
 			do
-					check_file=$(file $i | grep -i lsb | cut -d : -f 1)
+				check_file=$(file $i | grep -i lsb | cut -d : -f 1)
 			done
 
-	check_amass_installed=$?
-
-	if [ $check_amass_installed -ne 0 ]; then
-			echo "Amass is not there..."
-			# If 1 is returned from the function, then try another function which tries to install amass on the system.
+	check_ubuntu=$(uname -a | grep -iq ubuntu)
+	ubuntu_exist=$?
+	# If Amass is not installed.
+	if [ -z "$check_file" ] ; then
+		echo "Amass isn't installed. Attempting install on a Ubuntu OS."
+		# Check if the installed system is Ubuntu. We don't need to see the output so we use -q
+		if [ $ubuntu_exist -ne 0 ]; then
+			# If Ubuntu is not installed then quit.
+			echo "Your OS is not Ubuntu"			
 			return 1
-	else
-			# This value is returned from the function if all goes well.
-			echo "$check_file"
+		else
+			echo "OS is Ubuntu, downloading amass."
+			download_amass=$(wget https://github.com/OWASP/Amass/releases/download/v3.19.2/amass_linux_amd64.zip -O ~/amass.zip)
+			echo "Downloaded at: $(cd ~; pwd), unzipping: $(unzip ~/amass.zip)"
+			# We only care about one result.
+			find_amass=$(find ~ -type f -executable -name amass 2>/dev/null | tail -n 1)
+			echo "Available at: $find_amass"
+		fi
 	fi
 }
 
@@ -29,21 +38,19 @@ amass_scans()
 #echo "Scanning for SSL cert/hostnames on given IP address/es"
 #amass intel -active -addr $1 -timeout 1
 
-check_file=$(check_amass_installed)
+	check_amass_installed
 
-echo "Grabbing subdomains in passive mode"
-$check_file enum -passive -d $1 -src
+	find_amass=$(find / -type f -executable -name amass 2>/dev/null | tail -n 1)
+	echo "Grabbing subdomains in passive mode"
+	$find_amass enum -passive -d $1 -src
 
-echo "Grabbing further info in normal enum mode"
-$check_file enum -d $1 -o "amassoutput"
+	echo "Grabbing further info in normal enum mode"
+	$find_amass enum -d $1 -o "amassoutput"
 }
 
 root_domain=$1
 
 echo "Performing subdomain checks via Wayback"
 curl -s "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" |sort| sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^www\.//' | uniq
-
-amass_installed=$(check_amass_installed)
-echo $amass_installed
 
 amass_scans $1
