eb74b39e53c75a101a8497e7bd7cd2b6cd7c20fc
Aqeeb <aqeeb@aqeeb.com>
2022-06-12 01:23:55 +0100

    More comments involving code logic and fixes to do

--- code/recon/subdomain_recon.sh
+++ code/recon/subdomain_recon.sh
@@ -4,16 +4,13 @@
 # Author(s): Aqeeb Hussain, Mattia Campagnano #
 ###############################################
 
+
+# Check if amass is installed or not to determine installation.
 check_amass_installed()
 {
-	# Check if amass is installed or not to determine installation.
 	amasspath=$(find / -type f -executable -name amass 2>/dev/null)
 	
-	#for i in $amasspath
-	#do
-	#	check_file=$(file $i | grep -i lsb | cut -d : -f 1)
-	#done
-
+	# Check if the system is Ubuntu	
 	check_ubuntu=$(uname -a | grep -iq ubuntu)
 	ubuntu_exist=$?
 	# If Amass is not installed. Need to change this logic to cater for a source code installation via Go. 
@@ -24,36 +21,44 @@ check_amass_installed()
 			# If Ubuntu is not installed then quit.
 			echo "Your OS is not Ubuntu"
 			return 1
+		# If the OS is Ubuntu then download and install amass.
 		else
 			echo "OS is Ubuntu, downloading amass."
 			download_amass=$(wget https://github.com/OWASP/Amass/releases/download/v3.19.2/amass_linux_amd64.zip -O ~/amass.zip)
+
 			echo "Downloaded at: $(cd ~; pwd), unzipping: $(unzip ~/amass.zip)"
-			# We only care about one result.
+			# Find exact location of amass and then show this in a messagie
 			find_amass=$(find ~ -type f -executable -name amass 2>/dev/null | tail -n 1)
 			echo "Available at: $find_amass"
 		fi
 	fi
 }
 
+# Main function to conduct amass scans.
 amass_scans()
 {
-#echo "Scanning for SSL cert/hostnames on given IP address/es"
-#amass intel -active -addr $1 -timeout 1
 
+	# Function to check if amass even installed.
 	check_amass_installed
 
+	# Grab a copy of amass from the file system to use in the script.
 	find_amass=$(find / -type f -executable -name amass 2>/dev/null | tail -n 1)
 	echo "Grabbing subdomains in passive mode"
+	# Passively grab stuff, need to split this up using a switch of passive or aggressive.
 	$find_amass enum -passive -d $1 -src
 
+	# Enumerate normally and write to a file. 
 	echo "Grabbing further info in normal enum mode"
 	$find_amass enum -d $1 -o "amassoutput"
 
 }
 
+# The input parameter is the root domain. Need a usage function for this.
 root_domain=$1
 
 echo "Performing subdomain checks via Wayback"
+# Do some enumeration via wayback
 curl "http://web.archive.org/cdx/search/cdx?url=*.$root_domain/*&output=text&fl=original&collapse=urlkey" | sort | sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^www\.//' | uniq | tee -a wayback_domains.txt
 
+# Input parameter is the root doman to conduct the scans. 
 amass_scans $root_domain
