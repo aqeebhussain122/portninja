74b14ce054ab62e004037fa2e31a66d4819f43b1
Aqeeb H <35127065+aqeebhussain122@users.noreply.github.com>
2023-10-12 02:25:13 +0100

    Delete code/recon/ipv6/sender6.py

--- code/recon/ipv6/sender6.py
+++
@@ -1,6 +0,0 @@
-import socket
-# Create ipv6 datagram socket
-sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-# Allow own messages to be sent back (for local testing)
-sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
-sock.sendto("hello world".encode('utf-8'), ("ff02::abcd:1", 8080))
